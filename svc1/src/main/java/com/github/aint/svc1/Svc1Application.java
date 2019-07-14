d85d9f4bb7d64fcc849102a0cb0edab26c800444
Oleksandr Tyshkovets <olexandr.tyshkovets@gmail.com>
2019-07-14 23:09:28 +0300

    Extract restTemplate creation into separate config class

--- svc1/src/main/java/com/github/aint/svc1/Svc1Application.java
+++ svc1/src/main/java/com/github/aint/svc1/Svc1Application.java
@@ -6,16 +6,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RestController
 @SpringBootApplication
+@RequiredArgsConstructor
 public class Svc1Application {
+
+    private final RestTemplate restTemplate;
 
     @GetMapping
     public String rootApi() {
         log.info("Inside SVC_1");
-        return new RestTemplate().getForObject("http://localhost:8082/", String.class);
+        return restTemplate.getForObject("http://localhost:8082/", String.class);
     }
 
 	public static void main(String[] args) {
